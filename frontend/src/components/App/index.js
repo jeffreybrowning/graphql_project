a5cf4911b7a090b969aeba9933d3d27a8accd514
Jeffrey Browning <jeff.browning@bloomboard.com>
2017-09-13 15:22:46 -0700

    removes proxy code

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -9,7 +9,7 @@ class App extends Component {
   state = {data: []};
 
   get_current_loans = () => {
-    request.get(window.location.protocol + '//' + window.location.hostname + ':5000' + '/loans', (error, response, body) => {
+    request.get(window.location.protocol + '/loans', (error, response, body) => {
       this.setState({data: JSON.parse(body)})
     })
   };
